264469053f8120cac64ac8ff4e66118431c49716
Ayush Dixit <ayushdixit@Ayushs-MacBook-Air-2.local>
2025-08-29 21:48:04 +0530

    first commit

--- f.java
+++ f.java
@@ -1,77 +1,79 @@
-// import java.util.*;
+// import java.util.Scanner;
+
 // public class f {
+
 //     public static void main(String[] args) {
 //         Scanner sc=new Scanner(System.in);
-//         double base=sc.nextDouble();
-//         double height=sc.nextDouble();
-//         double area=0.5*base*height;
-//         double new_area=area/6.4516;
-//         System.out.println(new_area);
+//         int mark=sc.nextInt();
+//         int marks[]=new int[mark];
+//         // input 
+//         for(int i=0;i<mark;i++){
+//             marks[i]=sc.nextInt();
+//         }
+//          int x=sc.nextInt();
+
+//         for(int i=0;i<marks.length;i++){
+//             if(marks[i]==x){
+//                 System.out.print("found at index"+ " "+i);
+//             }
         
+//         }
 //     }
 // }
 
-// import java.util.*;
+// import java.util.Arrays;
+// import java.util.Scanner;
+
 // public class f {
 
-//   public static void main(String[] args) {
-//     Scanner sc=new Scanner(System.in);
-//     int no=sc.nextInt();
-    
-//     for(int n=2;n<no;n++){
-//       boolean isPrime=true;
-//     for(int i=2;i<n/2;i++){
-//          if(n%i==0){
-//           isPrime=false;
-//           break;
-//          }}
-//       if(isPrime){
-//          System.out.print(n+" ");
-         
-//          }
-         
-//     }
-//   }
-//   }
-
-// import java.util.*;
-// public class f{
 //     public static void main(String[] args) {
 //         Scanner sc=new Scanner(System.in);
-//         int n=sc.nextInt();
+//         int[] arr=new int[5];
+//         for(int i=0;i<arr.length;i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         System.out.print(Arrays.toString(arr));
+
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class f {
+
+//     public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+//         int[] arr=new int[5];
 //         int sum=0;
-//         for(int i=1;i<=n;i++){
-//             sum+=i;
+//         // input 
+//         for(int i=0;i<arr.length;i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         // output
+//         for(int i=0;i<arr.length;i++){
+//             sum+=arr[i];
 //         }
 //         System.out.print(sum);
 //     }
 // }
 
-
-// import java.util.*;
-// public class f{
-//     public static void main(String[] args) {
-//         Scanner sc=new Scanner(System.in);
-//         int n=sc.nextInt();
-//         int sum=0;
-//         while(n>0){
-//             int r=n%10;
-//             sum+=r;
-//             n=n/10;
+// public class f {
+//      public static void update(int marks[]){
+//         // loop for update the value
+//         for(int i=0;i<marks.length;i++){
+//             marks[i]+=1;
 //         }
-//         System.out.println(sum);
-// }}
+//      }
+//     public static void main(String[] args) {
+//         int marks[]={19,20,21};
+//          update(marks);
+//     for(int i=0;i<marks.length;i++){
+//         System.out.print(marks[i]+" ");
+//     }
+//     System.out.println();
+// }
 
-import java.util.*;
-public class f{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int year=sc.nextInt();
-        if((year%4==0 && year%100!=0)||(year%400==0)){
-           System.out.print("leap year");
-        }
-        else{
-            System.out.print("not a leap year");
-        }
-        
-}}
+//     }
+
+
+    
