7a0d9542531c941dff79743032475963691e5826
ayushdixit52 <ayushdixit583@gmail.com>
2025-11-19 14:40:44 +0530

    hshjdsa

--- assign-wrapper/Q5b.java
+++
@@ -1,6 +0,0 @@
-public class Q5b {
-    public static void main(String[] args) {
-        System.out.println(Character.toUpperCase('a'));
-    }
-}
-
--- oops-assign7/PaymentProcessor.java
+++
@@ -1,8 +0,0 @@
-public interface PaymentProcessor {
-    void processPayment(double amount);
-
-    // New default refund method
-    default void refund(double amount) {
-        System.out.println("Default refund processing for amount: " + amount);
-    }
-}
